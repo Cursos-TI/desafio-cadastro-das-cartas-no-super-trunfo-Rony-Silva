7bfe861e7fad648294c042e106579b97d086766b
Rony-Silva <54727806+Rony-Silva@users.noreply.github.com>
2025-08-15 03:15:48 +0000

    Densidade Populacional e PIB per Capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -24,6 +24,7 @@ float pop=0;        // Número com casa decimal.
 float area=0;
 float pib=0;
 int npt=0;         //Número inteiro.
+float dens=0, pibper =0;
 
 printf("Informe o Estado:\n" ); //Imprime na tela
      fgets(estado,EST,stdin);        //Captura informação do teclado
@@ -59,14 +60,24 @@ printf("Informe o PIB:\n" );
 printf("Informe o numeros de Pontos turisticos:\n" );
     	scanf("%d",&npt);
         getchar();
+
+// Operação Aritimética
+dens = (pop / area);
+pibper = (pib / pop);
+
 printf("\n***[Carta 1]***\n");
+
+// Imprime o resultado 
     
-printf("Estado : %s\nCódigo do Estado : %s\nCidade : %s\nQuantidade populacional é de %.3f Habitantes\nExtensão Geográfica é de %.3f Km²\nProduto interno bruto é de R$ %.1f milhões\nQuantidade de pontos Turísticos: %d\n",estado,cod,cid,pop,area,pib,npt);
+printf("Estado: %s\nCódigo do Estado: %s\nCidade: %s\nPopulacao: %.3f Habitantes\nExtensão Geográfica: %.3f Km²\nProduto interno bruto: R$ %.1f milhões\nQuantidade de pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.3f reais. ",estado,cod,cid,pop,area,pib,npt,dens,pibper);
 
-printf("=========================================================================================//=================================================================================================\n\n");
- 
+// Separador
+printf("\n=========================================================================================//=================================================================================================\n\n");
+ // limpa a variável
+dens =0;
+pibper =0;
 
-printf("Informe o Estado:\n");
+ printf("Informe o Estado:\n");
     fgets(estado,EST,stdin);
     fflush(stdin);	
     estado[strlen(estado)-1] = '\0';
@@ -107,10 +118,15 @@ printf("Informe o PIB:\n" );
 printf("Informe o numeros de Pontos turisticos:\n" );
     	scanf("%d",&npt);
         getchar();
+
+// Operação Aritimética
+dens = (pop / area);
+pibper = (pib / pop);
+       
    
 printf("\n***[Carta 2]***\n");
     
-printf("Estado :%s\nCódigo do Estado : %s\nCidade : %s\nQuantidade populacional é de %.3f Habitantes\nExtensão Geográfica é de %.3f Km²\nProduto interno bruto é de R$ %.1f milhões\nQuantidade de pontos Turísticos: %d",estado,cod,cid,pop,area,pib,npt);
+printf("Estado :%s\nCódigo do Estado : %s\nCidade : %s\nQuantidade populacional é de %.3f Habitantes\nExtensão Geográfica é de %.3f Km²\nProduto interno bruto é de R$ %.1f milhões\nQuantidade de pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.3f reais. ",estado,cod,cid,pop,area,pib,npt,dens,pibper);
 
 
     return 0;     // Retorna 0
